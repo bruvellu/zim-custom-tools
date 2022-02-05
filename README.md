a4aa7890ae1b48adfadf4f1f810dabb039f203f4
Bruno Vellutini <organelas@gmail.com>
2022-02-06 00:20:35 +0100

    Initial commit with list sub-pages custom tool

---
+++ README.md
@@ -0,0 +1,6 @@
+# :wrench: Zim Custom Tools
+
+My collection of custom tools for [Zim Desktop Wiki](https://zim-wiki.org/).
+
+- [zim_list_subpages.sh](zim_list_subpages.sh): Creates a list with links to
+  all sub-pages under your cursor.
---
+++ zim_list_subpages.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Zim Custom Tool to generate a list of sub-pages (with links).
+
+# Install:
+#
+# 1. Copy this script somewhere
+# 2. Open Zim > Tools > Custom Tools
+# 3. Add new custom tool
+# 4. Fill in details
+#   a. Name: List Sub-Pages
+#   b. Description: Create list with links to sub-pages 
+#   c. Command: ~/somewhere/zim_list_subpages.sh %d
+# 5. Check "Output should replace current selection"
+# 6. Go to Preferences > Key bindings and set CTRL+ALT+L to
+#    <Actions>/custom_tools/list sub-pages-usercreated
+#
+# Note: "%d" stands for the attachment directory of the current page.
+
+# Usage:
+#
+# 1. Open a page that has sub-pages
+# 2. Put your cursor where you want your list
+# 3. Go to Tools > List Sub-Pages
+# 4. An unformatted list will appear
+# 5. Reload the page to render the links (CTRL+R)
+#
+# Note: If there are no sub-pages, nothing will be printed.
+
+# Get attachment directory of current page
+DIR=$1
+
+# Get sub-pages in directory
+for i in `ls ${DIR}/*.txt`; do
+  # Strip extension out of page name
+  subpage=`basename ${i} .txt`
+  # Replace underscores with spaces
+  subpage=${subpage//_/ }
+  # Generate list item with link to sub-page
+  echo "* [[+${subpage}|${subpage}]]"
+done
+
+# TODO:
+#
+# - Option to use numbered lists
+# - Recursive sub-pages with indentation
+# - Option to reverse sort and sort by date
