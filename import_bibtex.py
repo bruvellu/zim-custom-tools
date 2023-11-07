5f46e64602eea9dceefd6edf0a077297e40baa4c
Bruno Vellutini <organelas@gmail.com>
2023-11-08 00:13:50 +0100

    Refactor code to print fields properly

--- import_bibtex.py
+++ import_bibtex.py
@@ -36,9 +36,10 @@
 
 # biber --tool -V paperpile.bib > biberrors.txt
 
+from collections import OrderedDict
 from datetime import datetime
-from pybtex.database import parse_file
 from pathlib import Path
+from pybtex.database import parse_file
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -55,59 +56,101 @@ Path.mkdir(root, exist_ok=True)
 print(f'Converting "{bibfile}" to "{root}" directory...')
 
 # Page template
-template = '''Content-Type: text/x-zim-wiki
+template = '''\
+Content-Type: text/x-zim-wiki
 Wiki-Format: zim 0.6
 Creation-Date: {isodate}
 
 ====== {bibkey} ======
 Created {subdate}
 
-{entry}
+@{bibtype}
 '''
 
 # Read and parse bibtex file
 bibdata = parse_file(bibfile)
 
 # Loop over entries in bibtex file
-for bibkey, values in bibdata.entries.items():
+for bibkey, entry in bibdata.entries.items():
 
     # Skip references without author
     if bibkey.startswith('noauthor'):
         continue
 
-    # Define page folder using first author letter
-    folder = root / bibkey[0]
+    # Print bibkey to terminal
+    print(bibkey)
+
+    # Define subfolder using the bibkey's first letter
+    subfolder = bibkey[0]
+
+    # Define path for subfolder
+    subpath = root / subfolder
+
+    # Create subfolder (A, B, C, etc...)
+    Path.mkdir(subpath, exist_ok=True)
+
 
     # Define filename using the bibkey
     filename = f'{bibkey}.txt'
 
-    # Define filepath
-    filepath = folder / filename
-
-    # Create directory
-    Path.mkdir(folder, exist_ok=True)
+    # Define full file path
+    filepath = subpath / filename
 
     # Create file using bibkey as name
     zimfile = open(filepath, 'w')
 
-    # Save entry values in bibtex format
-    entry = values.to_string('bibtex')
+
+    # Get entry type
+    bibtype = entry.type
+
+    # Get authors list (key + ordered dictionary)
+    bibpersons = entry.persons
+
+    # Get entry fields (ordered dictionary)
+    bibfields = OrderedDict(entry.fields)
+
+    # Get full entry in BibTeX format
+    bibtex = entry.to_string('bibtex')
+
 
     # Get datetime now
     now = datetime.now()
     isodate = now.astimezone().replace(microsecond=0).isoformat()
     subdate = now.strftime('%A %d %B %Y')
 
-    # Save final content of the page
-    page = template.format(bibkey=bibkey, entry=entry, isodate=isodate,
-            subdate=subdate)
+    # Save main page content
+    page = template.format(bibkey=bibkey,
+                           bibtype=bibtype,
+                           isodate=isodate,
+                           subdate=subdate)
+
+    # Loop for bypassing the author key
+    person_dict = {}
+    for person_key, persons in bibpersons.items():
+        person_list = [str(person) for person in persons]
+        person_string = '; '.join(person_list)
+        person_dict.update({person_key: person_string})
+
+    # Add authors to the fields dictionary at the second position
+    bibfields.update(person_dict)
+    if 'editor' in person_dict.keys():
+        bibfields.move_to_end('editor', last=False)
+    if 'author' in person_dict.keys():
+        bibfields.move_to_end('author', last=False)
+    bibfields.move_to_end('title', last=False)
+
+
+    # Print fields to page
+    for field, value in bibfields.items():
+        page = page + f'\n**{field}:** {value}'
+
+    # Add raw BibTeX at the end
+    # page = page + f"\n\n'''\n{bibtex}'''"
 
     # Write page to file (returns number of characters written)
     characters = zimfile.write(page)
 
+
     # Close file
     zimfile.close()
-
-    # Print bibkey to terminal
-    print(bibkey)
 
